30255a24c1f0498c3f511dbc4a856d233739cbcf
Voskakis <constantinosvoskakis@hotmail.gr>
2025-02-16 17:24:58 +0200

    Decoupling ProductsService from RestApiSettings gets HttpClient from DI

--- src/CSharpApp.Application/Products/ProductsService.cs
+++ src/CSharpApp.Application/Products/ProductsService.cs
@@ -3,25 +3,21 @@ namespace CSharpApp.Application.Products;
 public class ProductsService : IProductsService
 {
     private readonly HttpClient _httpClient;
-    private readonly RestApiSettings _restApiSettings;
     private readonly ILogger<ProductsService> _logger;
 
-    public ProductsService(IOptions<RestApiSettings> restApiSettings, 
-        ILogger<ProductsService> logger)
+    public ProductsService(IOptions<RestApiSettings> restApiSettings, ILogger<ProductsService> logger, HttpClient client)
     {
-        _httpClient = new HttpClient();
-        _restApiSettings = restApiSettings.Value;
+        _httpClient = client;
         _logger = logger;
     }
 
     public async Task<IReadOnlyCollection<Product>> GetProducts()
     {
-        _httpClient.BaseAddress = new Uri(_restApiSettings.BaseUrl!);
-        var response = await _httpClient.GetAsync(_restApiSettings.Products);
+        var response = await _httpClient.GetAsync(string.Empty);
         response.EnsureSuccessStatusCode();
         var content = await response.Content.ReadAsStringAsync();
         var res = JsonSerializer.Deserialize<List<Product>>(content);
-        
+
         return res.AsReadOnly();
     }
 }
