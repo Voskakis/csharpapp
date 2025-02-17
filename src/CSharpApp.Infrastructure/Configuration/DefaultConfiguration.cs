59ac157623b8b23c25bceceba4c43b39c580763e
Voskakis <constantinosvoskakis@hotmail.gr>
2025-02-17 02:36:26 +0200

    swapped service type for AuthService to scoped

--- src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/DefaultConfiguration.cs
@@ -12,7 +12,7 @@ public static class DefaultConfiguration
 
         services.AddSingleton<IProductsService, ProductsService>();
         services.AddSingleton<ICategoriesService, CategoriesService>();
-        services.AddSingleton<IAuthService, AuthService>();
+        services.AddScoped<IAuthService, AuthService>();
 
         return services;
     }
